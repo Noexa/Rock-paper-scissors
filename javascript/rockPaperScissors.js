7dbef42186a347f52954249221bd0e87554b29f1
Garrett Brunsch <69402368+Noexa@users.noreply.github.com>
2024-03-27 03:47:17 -0700

    Add a function to convert the computer's answer to text and display it's choice to the user

--- javascript/rockPaperScissors.js
+++ javascript/rockPaperScissors.js
@@ -1,13 +1,32 @@
 let userInput = prompt("It's time for a game of rock, paper, scissors! Please type which move you'd like to use: ")
-console.log(userInput)
-
 let ignoreCapitalization = userInput.toLowerCase()
 
 function getComputerChoice () {
     let computerChoice = Math.floor(Math.random()*3)+1
-    console.log(computerChoice)
-    return computerChoice
+    function convertComputer(){
+        if (computerChoice===1) {
+            computerChoice="rock"
+            console.log("The computer chose: " + computerChoice)
+            return computerChoice
+        }
+        else if (computerChoice===2){
+            computerChoice="paper"
+            console.log("The computer chose: " + computerChoice)
+            return computerChoice
+        }
+        else if (computerChoice===3){
+            computerChoice="scissors"
+            console.log("The computer chose: " + computerChoice)
+            return computerChoice
+        }
+
+    }
+   convertComputer()
+
+// call on this later to display the results
+
 }
+
 getComputerChoice()
 
 function showResult () {
