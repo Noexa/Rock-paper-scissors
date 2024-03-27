e76645f53f5f4b1bc7d2f5894dd5d947d5410bee
Garrett Brunsch <69402368+Noexa@users.noreply.github.com>
2024-03-27 03:01:10 -0700

    Add a prompt for the user's move and store the information provided

--- javascript/rockPaperScissors.js
+++ javascript/rockPaperScissors.js
@@ -1,5 +1,10 @@
+let userInput = prompt("It's time for a game of rock, paper, scissors! Please type which move you'd like to use: ")
+console.log(userInput)
+//still needs to be case insenstive
+
 function getComputerChoice () {
     let computerChoice = Math.floor(Math.random()*3)+1
     console.log(computerChoice)
+    return computerChoice
 }
 getComputerChoice()
