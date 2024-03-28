fa255b42f95f7cb73446f71fd379d4cd2a1fcc7e
Garrett Brunsch <69402368+Noexa@users.noreply.github.com>
2024-03-28 02:36:37 -0700

    Fix previous bug and remove obsolete comments

--- javascript/rockPaperScissors.js
+++ javascript/rockPaperScissors.js
@@ -1,6 +1,5 @@
 //Alerts user about the game and asks for their move
-let userInput = prompt("It's time for a game of rock, paper, scissors! Please type which move you'd like to use: ")
-let ignoreCapitalization = userInput.toLowerCase()
+let userInput = prompt("It's time for a game of rock, paper, scissors! Please type which move you'd like to use: ").toLowerCase()
 
 //Automatically assigns a move to the computer
 function getComputerChoice () {
@@ -20,36 +19,35 @@ return convertToWord(computerChoice)
 }
 //Stores computerChoice in global variable
 let computerChoice = getComputerChoice()
-//console.log(computerChoice)
 
 //Displays the user's choice. Possibly rewrite to include total answer
-function showResult () {
-    if (ignoreCapitalization===computerChoice){
-        console.log("You both selected " + ignoreCapitalization + ". Tie game! ")
+function showResult(userInput, computerChoice) {
+    if (userInput===computerChoice){
+        console.log("You both selected " + userInput + ". This round is a tie!")
         return ("tie")
 }
 
-    else if (ignoreCapitalization==="rock" && computerChoice==="scissors"){
+    else if (userInput==="rock" && computerChoice==="scissors"){
         console.log("You've selected rock and your opponent picked scissors. You win!")
         return ("win")
     }
-    else if (ignoreCapitalization==="paper" && computerChoice==="rock"){
+    else if (userInput==="paper" && computerChoice==="rock"){
         console.log("You've selected paper and your opponent picked rock. You win!")
         return ("win")
     }
-    else if (ignoreCapitalization==="scissors" && computerChoice==="paper"){
+    else if (userInput==="scissors" && computerChoice==="paper"){
         console.log("You've selected scissors and your opponent picked paper. You win!")
         return ("win")
     }
-    else if (ignoreCapitalization==="paper" && computerChoice==="scissors"){
+    else if (userInput==="paper" && computerChoice==="scissors"){
         console.log("You've selected paper and your opponent picked scissors. You lose!")
         return ("lose")
     }
-    else if (ignoreCapitalization==="rock" && computerChoice==="paper"){
+    else if (userInput==="rock" && computerChoice==="paper"){
         console.log("You selected rock and your opponent picked paper. You lose!")
         return ("lose")
     }
-    else if (ignoreCapitalization==="scissors" && computerChoice==="rock") {
+    else if (userInput==="scissors" && computerChoice==="rock") {
         console.log("You picked scissors and your opponent picked rock. You lose!")
         return ("lose")
     }
@@ -58,38 +56,41 @@ function showResult () {
         return ("failed to enter")
     
 }
-//Console log might need to be removed when you revisit this project, but for now it's fine
-showResult()
-console.log("Time for bonus game! It's a best of 5. Good luck!")
+//Displays results of the single round
+showResult(userInput, computerChoice)
+console.log(" ")
+console.log("Time for a bonus game! The game will last five rounds; good luck!")
 
-//playgame() function that uses an array to check the validity of user inputs and records a bo5
+//playgame() function that uses an array to check the validity of user inputs and records 5 rounds of rock, paper, scissors
  function playGame(){
     let playerScore = 0
     let computerScore = 0
-    for (i=0; i<5; i++){
-        let userInput = prompt("Please type your move: ")
-        let ignoreCapitalization = userInput.toLowerCase()
+    for (i=1; i<6; i++){
+        let userInput = prompt("Please type your move for round #" + i +": ").toLowerCase()
+
         if (!["rock", "paper", "scissors"].includes(userInput)) {
             console.log("You did not enter a valid response. Please check your spelling and try again.")
         i--
         continue
     }
-    //Currently bugged. Does not update variables and copies results of the initial game prior to bo5. Need to declare and assign new variables?
-    showResult(ignoreCapitalization)
-        if (showResult==="tie"){
-            playerScore++
-            computerScore++
+        let computerChoice = getComputerChoice()
+        let result = showResult(userInput, computerChoice)
+            if (result === "tie"){
+                playerScore++
+                computerScore++
     }
-        else if (showResult==="win"){
-            playerScore++
+            else if (result === "win"){
+                playerScore++
     }
-        else if (showResult==="lose"){
-            computerScore++
+            else if ( result ==="lose"){
+                computerScore++
     }
-    console.log("The current score is "+playerScore + ":" +computerScore+".")
+        if (i<5){ console.log("The current score is "+playerScore + ":" +computerScore+".")}
+        else if (i===5){ console.log("The final score is "+playerScore + ":" +computerScore+".")}
 
     }
-
+    console.log(" ")
+    console.log("Thanks for playing!")
  }
 playGame()
 
